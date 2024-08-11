a08a31f6f823aef80750ecda69a15794c1c2d548
vanhalenpanama <96343092+vanhalenpanama@users.noreply.github.com>
2024-08-11 15:51:41 +0900

    :truck: file omission

--- tube/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("signup/", views.user_signup, name="user_signup"),
-    path("login/", views.user_login, name="user_login"),
-    path("logout/", views.user_logout, name="user_logout"),
-    path("profile/", views.user_profile, name="user_profile"),
-]
